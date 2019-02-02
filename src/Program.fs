117372f5c5939a9594a184b5b8d20ca68ab5a24c
Jeff Boek <jeff@jeffboek.com>
2019-02-02 15:45:37 -0800

    Fancy first commit

---
+++ src/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.IO
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+
+let configureApp (app: IApplicationBuilder) =
+    app.Run (fun ctx ->
+        printfn "%A - %A - %A - %A" ctx.Request.Method ctx.Request.PathBase ctx.Request.Path ctx.Request.QueryString
+        sprintf "Hello world" |> ctx.Response.WriteAsync  
+)
+
+[<EntryPoint>]
+let main argv =
+    WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .Build()
+        .Run()
+
+    0 // return an integer exit code
+
