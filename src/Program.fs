fb156157941387102a8ad6ca2fc229b16f909a64
Jeff Boek <jeff@jeffboek.com>
2019-02-09 16:01:31 -0800

    Adds request errors

--- src/Program.fs
+++ src/Program.fs
@@ -1,10 +1,21 @@
 ﻿
 open Suave
+open Suave.Filters
+open Suave.Successful
+open Suave.Operators
+open Suave.RequestErrors
+
+
+let app =
+    choose [
+        path "/" >=> (OK "/")
+        NOT_FOUND "404"
+    ]
 
 [<EntryPoint>]
 let main argv =
     let binding = HttpBinding.createSimple HTTP "0.0.0.0" 8081
     let config = { defaultConfig with bindings = [ binding ] }
-    startWebServer config (Successful.OK "hello world")
+    startWebServer config app
 
     0 // return an integer exit code
